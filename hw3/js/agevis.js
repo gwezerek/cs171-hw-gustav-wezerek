4d35b6e02aa52ae9683a2f7ab4a336c77bc0e68d
Gus Wezerek <gwezerek@gmail.com>
2015-03-29 22:20:21 -0400

    Swapped out dummy default data for ageViz.

--- hw3/js/agevis.js
+++ hw3/js/agevis.js
@@ -6,7 +6,7 @@ AgeViz = function( _parentElement, _data, _metaData ){
     this.parentElement = _parentElement;
     this.data = _data;
     this.metaData = _metaData;
-    this.displayData = [7, 24, 18, 11, 23, 26, 28, 40, 38, 89, 340, 396, 739, 982, 3336, 10020, 10042, 71731, 10106, 8414, 9157, 7803, 7287, 9832, 5048, 5897, 3844, 3593, 3461, 2589, 3321, 1913, 2160, 32935, 1619, 2010, 1324, 1183, 1271, 1064, 3641, 851, 1001, 946, 751, 906, 686, 1065, 642, 584, 7672, 506, 540, 524, 482, 496, 445, 371, 1452, 316, 414, 288, 297, 297, 277, 289, 252, 2328, 197, 208, 201, 177, 165, 1378, 125, 120, 103, 107, 109, 112, 148, 101, 105, 105, 109, 113, 121, 121, 146, 266, 158, 128, 152, 139, 142, 143, 128, 116, 125, 252];
+    this.displayData = [];
 
     this.initVis();
 }
@@ -52,7 +52,8 @@ AgeViz.prototype.initVis = function(){
         .attr( 'class', 'age-area-wrap' )
         .attr( 'transform', 'translate(' + this.width + ', 0)' );
 
-    this.updateVis();
+    var timeExtent = d3.extent( this.data, function( d ) { return d.time; } );
+    this.onSelectionChange( timeExtent[0], timeExtent[1] );
 }
 
 AgeViz.prototype.updateVis = function() {
@@ -76,6 +77,7 @@ AgeViz.prototype.updateVis = function() {
 }
 
 AgeViz.prototype.onSelectionChange = function( selectionStart, selectionEnd ) {
+  console.log(arguments);
     this.displayData = this.filterAndAggregate( selectionStart, selectionEnd );
     this.updateVis();
 }
@@ -86,9 +88,11 @@ AgeViz.prototype.filterAndAggregate = function( from, to ) {
     var perDayMap = d3.map( this.data, function( val ) { return val.time; } );
 
     $.each( dateArr, function( index, value ) {
-        $.each( perDayMap.get( value ).ages, function( j, ageVal ) {
-            res[ j ] += ageVal;
-        });
+        if ( perDayMap.get( value ) ) {
+          $.each( perDayMap.get( value ).ages, function( j, ageVal ) {
+              res[ j ] += ageVal;
+          });
+        }
     });
 
     return res;
